d5745669b7eb15f88f06aab9fa0c534b4b572326
Donggeon Im <dgi990101@gmail.com>
2026-07-31 17:48:50 +0900

    started weather 1 mock up

---
+++ src/components/Weather1.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+const search_input = ref('')
+</script>
+
+<template>
+  <div class="weather1">
+    <h2 class="weather1_title">과제 1: 날씨 (Mockup)</h2>
+    <br />
+    <hr />
+    <section class="search_box">
+      <h3>도시 검색</h3>
+      <input
+        type="text"
+        :value="search_input"
+        @input="(e) => (search_input = e.target.value)"
+        placeholder="검색할 도시 이름 입력"
+      />
+    </section>
+  </div>
+</template>
+
+<style scoped></style>
